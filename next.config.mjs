5b0dc03c01dd1e1b6ce61f8efcb1d48fd9323a65
raghul <raghulkrishna50@gmail.com>
2024-05-25 15:20:49 +0530

    check_2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/portfolio",
-    // output: "export",
+    reactStrictMode: true,
     images: {
         domains: ['skillicons.dev']
     }
